bfcd6d1b3103a38d3f8f31d95ffb06c3a82dc16e
Daniel <daniel_e_dapper@estudante.sesisenai.org.br>
2025-11-16 00:01:56 -0300

    Feat: Atualização da Classe Pedagógico / Criação de Enum Idioma

--- src/Models/Gestao/Idioma.java
+++ src/Models/Gestao/Idioma.java
@@ -1,29 +1,6 @@
 package Models.Gestao;
 
-public class Idioma
+public enum Idioma
 {
-    private Long idIdioma;
-    private String idioma;
-
-    public Idioma(Long idIdioma, String idioma)
-    {
-        this.idIdioma = idIdioma;
-        this.idioma = idioma;
-    }
-
-    public Idioma(String idioma)
-    {
-        this(null, idioma);
-    }
-
-    public String getIdioma() {
-        return idioma;
-    }
-    public Long getIdIdioma() {
-        return idIdioma;
-    }
-
-    public void setIdioma(String idioma) {
-        this.idioma = idioma;
-    }
+    INGLES, ESPANHOL, ALEMAO, ITALIANO, FRANCES;
 }
